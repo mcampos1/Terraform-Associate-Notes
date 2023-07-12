c336e0b4d1e429e15960ce0904a1fb96464b1d18
Martin Campos <martin.campos8@gmail.com>
2023-07-12 15:15:23 -0400

    Create first_ec2.tf

---
+++ first_ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+resource "aws_instance" "web" {
+  ami = ami-04823729c75214919
+  instance_type = "t2.micro"
+  }
