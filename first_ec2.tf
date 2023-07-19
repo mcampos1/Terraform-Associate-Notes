fa2e881d62c21e93ec2e1adda61cd3ac926cbc71
Martin Campos <martin.campos8@gmail.com>
2023-07-19 13:47:15 -0400

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -12,5 +12,16 @@ resource "aws_instance" "myec2" {
       Name = "first ec2"
   }
 
+#putting 2 resources of the same name results in an error when running terraform plan
+#resource names must be unique per type in each module
+
+resource "aws_instance" "myec2" {
+  ami = ami-04
+  instance_type = "t2.micro"
+
+  tags = {
+      Name = "first ec2"
+  }
+
 provider azurerm {}
 #this is enough for terraform to download the appriopriate plugins with terraform init
