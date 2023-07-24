ea12e8aa777a889ea3ee0e75dfa4547515284d08
Martin Campos <martin.campos8@gmail.com>
2023-07-24 12:46:31 -0400

    Update github.tf

--- github.tf
+++ github.tf
@@ -10,3 +10,10 @@ terraform {
 provider "github" {
   token = "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"
 }
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+}
