d8d8a09597e786bc77a47b57018c2c0a9e15982a
Martin Campos <martin.campos8@gmail.com>
2023-08-29 14:19:39 -0400

    Update eip&sg.tf

--- eip&sg.tf
+++ eip&sg.tf
@@ -1,21 +1,18 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  region     = "us-east-1"
 }
 
-resource "aws_eip" "myeip" {
+resource "aws_eip" "lb" {
   domain = "vpc"
   }
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
   
   ingress {
     description      = "TLS from VPC"
     from_port        = "443"
     to_port          = "443"
     protocol         = "tcp"
-    cidr_blocks      = [aws_eip.myeip.public]
+    cidr_blocks      = [aws_eip.lb.public_ip]
