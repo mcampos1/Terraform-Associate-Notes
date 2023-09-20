cafd3b2ba673fd26478b2a097760005e85933707
Martin Campos <martin.campos8@gmail.com>
2023-09-20 11:40:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,12 @@ variable "vpn_ip" {
   default = "116.50.30.20/32"
 } 
 #environment variables
-variable "instance_type" {
+variable "instancetype" {
    default = "t2.micro"
 }
-#command line flag: terraform plan -var
+#command line flag 
+
+    terraform plan -var="instancetype=t2.small"
+
 #running terraform plan with no default value will prompt you to input the value for that variable.
-variable "instance_type" {}
+variable "instancetype" {}
