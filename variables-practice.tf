a9064ef95b1816da1d16ccca15e849136cd528ff
Martin Campos <martin.campos8@gmail.com>
2023-09-20 11:22:11 -0400

    Update variables-practice.tf

--- variables-practice.tf
+++ variables-practice.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
 
+resource "aws_instance" "myec2" {
+  ami = "ami-082b5a644766e0e6f"
+  instance_type = var.instancetype
+}
