ea5a1e7db7f326db640bf93346f14ce770637dcf
Martin Campos <martin.campos8@gmail.com>
2023-09-20 11:23:59 -0400

    Update variables-practice.tf

--- variables-practice.tf
+++ variables-practice.tf
@@ -3,8 +3,9 @@ provider "aws" {
   access_key = "my-access-key"
   secret_key = "my-secret-key"
 }
-
+#input a variable instead of hardcoding
 resource "aws_instance" "myec2" {
   ami = "ami-082b5a644766e0e6f"
   instance_type = var.instancetype
 }
+
