cc8e94a60a4ecb523b5dd4b9f6c53d1b48784a36
Martin Campos <martin.campos8@gmail.com>
2023-11-27 14:05:05 -0500

    Update conditional.tf

--- conditional.tf
+++ conditional.tf
@@ -1,1 +1,15 @@
-provider 
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+resource "aws_instance" "dev" {
+  ami = ami-04823729c75214919
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "prod" {
+  ami = ami-04823729c75214919
+  instance_type = "t2.micro"
+}
