dfbf9759e6a6371a8446a5cb2fd1206e9558efda
Martin Campos <martin.campos8@gmail.com>
2023-11-27 14:12:51 -0500

    Update conditional.tf

--- conditional.tf
+++ conditional.tf
@@ -4,15 +4,18 @@ provider "aws" {
   secret_key = "my-secret-key"
 }
 
-variable "is-test" {} 
+variable "istest" {} 
 
 resource "aws_instance" "dev" {
   ami = ami-04823729c75214919
   instance_type = "t2.micro"
-  count = var.is-test == true #conditional variable
+  count = var.istest == true ? 1 : 0 
+#if var.istest is true then count=1 else count=0 and no ec2 instance will be created from that specific block
 }
 
 resource "aws_instance" "prod" {
   ami = ami-04823729c75214919
   instance_type = "t2.micro"
+  count = var.istest == false ? 1 : 0
+#if var.istest is false then instance will be created from this specific block else no prod ec2-instance
 }
