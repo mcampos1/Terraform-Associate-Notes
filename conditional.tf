e7481a4778031dd5c783887dc75236ddccb738c9
Martin Campos <martin.campos8@gmail.com>
2023-11-27 14:07:45 -0500

    Update conditional.tf

--- conditional.tf
+++ conditional.tf
@@ -9,6 +9,7 @@ variable "is-test" {}
 resource "aws_instance" "dev" {
   ami = ami-04823729c75214919
   instance_type = "t2.micro"
+  count = var.is-test == true #conditional variable
 }
 
 resource "aws_instance" "prod" {
